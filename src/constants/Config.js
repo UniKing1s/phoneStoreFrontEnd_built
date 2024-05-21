b5f6c337b4313db0baf3d678d69c4b7be78e639b
UniKing1s <khoivo050603it@gmail.com>
2024-05-21 12:03:34 +0700

    update api

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:5000";
-export const API_URL = "https://backendphonestore.onrender.com";
+export const API_URL = "https://backendphonestore-0gx0.onrender.com";
 // export const API_URL = process.env.normal_url;
 // export const imgAPI_URL = "http://localhost:5000/images/";
-export const imgAPI_URL = "https://backendphonestore.onrender.com/images/";
+export const imgAPI_URL = `${API_URL}/images/`;
 // export const imgAPI_URL = process.env.img_url;
